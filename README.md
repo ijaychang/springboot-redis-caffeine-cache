673f2a931aa5fd459000ae8fc3f094ca24456f56
jaychang <jaychang1987@gmail.com>
2024-12-16 15:32:29 +0800

    feat: value序列化改用jackson来实现

--- README.md
+++ README.md
@@ -497,67 +497,6 @@ public class CaffeineCacheService {
 
 <br>
 
-#### 5.覆盖com.jincou.core.starter.CacheRedisCaffeineAutoConfiguration.stringKeyRedisTemplate 生成的 RedisTemplate
-关键是@Bean(name = "stringKeyRedisTemplate") 要指定下beanName
-```java
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import com.jincou.core.starter.CacheRedisCaffeineAutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-@AutoConfigureBefore({RedisAutoConfiguration.class, CacheRedisCaffeineAutoConfiguration.class})
-public class RedisConfig {
-
-    @Bean(name = "stringKeyRedisTemplate")
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-
-        // 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        // 指定要序列化的域(field,get,set)，访问修饰符(public,private,protected)
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 修复com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class org.springframework.cache.support.NullValue and no properties discovered to create BeanSerializer (to avoid exception, disable SerializationFeature.FAIL_ON_EMPTY_BEANS) 报错
-        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
-
-        // Validator验证类用于验证是否能够被反序列化,DefaultTyping指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-
-        // key采用String的序列化方式
-        redisTemplate.setKeySerializer(stringRedisSerializer);
-        // hash的key也采用String的序列化方式
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        // value序列化方式采用jackson
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash的value序列化方式采用jackson
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-
-        redisTemplate.afterPropertiesSet();
-        return redisTemplate;
-    }
-
-}
-
-```
-
 ## 推荐相关二级缓存相关项目
 
 1.阿里巴巴jetcache: https://github.com/alibaba/jetcache
--- redis-caffeine-cache-starter/src/main/java/com/jincou/core/starter/CacheRedisCaffeineAutoConfiguration.java
+++ redis-caffeine-cache-starter/src/main/java/com/jincou/core/starter/CacheRedisCaffeineAutoConfiguration.java
@@ -2,6 +2,15 @@ package com.jincou.core.starter;
 
 
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.jincou.core.cache.RedisCache;
 import com.jincou.core.config.L2CacheProperties;
 import com.jincou.core.spring.RedisCaffeineCacheManager;
@@ -13,6 +22,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.support.NullValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -20,10 +30,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
+import java.io.IOException;
 import java.net.UnknownHostException;
 
 
@@ -51,12 +62,41 @@ public class CacheRedisCaffeineAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean(name = "stringKeyRedisTemplate")
 	public RedisTemplate<Object, Object> stringKeyRedisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
-		RedisTemplate<Object, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory);
-		RedisSerializer stringSerializer = new StringRedisSerializer();
-		template.setKeySerializer(stringSerializer);
-		template.setHashKeySerializer(stringSerializer);
-		return template;
+		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		// 用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+		Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		// 指定要序列化的域(field,get,set)，访问修饰符(public,private,protected)
+		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		// Validator验证类用于验证是否能够被反序列化,DefaultTyping指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+		objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+
+		// 增加对NullValue的处理
+		SimpleModule nullValueSimpleModule = new SimpleModule();
+		nullValueSimpleModule.addSerializer(NullValue.class, NullValueSerializer.INSTANCE);
+		nullValueSimpleModule.addDeserializer(NullValue.class, NullValueDeserializer.INSTANCE);
+		objectMapper.registerModule(nullValueSimpleModule);
+
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+		StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+		// key采用String的序列化方式
+		redisTemplate.setKeySerializer(stringRedisSerializer);
+		// hash的key也采用String的序列化方式
+		redisTemplate.setHashKeySerializer(stringRedisSerializer);
+		// value序列化方式采用jackson
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		// hash的value序列化方式采用jackson
+		redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
 	}
 
 	@Bean
@@ -78,5 +118,35 @@ public class CacheRedisCaffeineAutoConfiguration {
 		RedisCache redisCache = new RedisCache();
 		redisCache.setRedisTemplate(stringKeyRedisTemplate);
 		return redisCache;
+	}
+
+	public static class NullValueDeserializer extends StdDeserializer<NullValue> {
+
+		public static final NullValueDeserializer INSTANCE = new NullValueDeserializer();
+
+		protected NullValueDeserializer() {
+			super(NullValue.class);
+		}
+
+		@Override
+		public NullValue deserialize(JsonParser p, DeserializationContext ctx) {
+			return (NullValue) NullValue.INSTANCE;
+		}
+
+	}
+
+	public static class NullValueSerializer extends StdSerializer<NullValue> {
+
+		public static final NullValueSerializer INSTANCE = new NullValueSerializer();
+
+		protected NullValueSerializer() {
+			super(NullValue.class);
+		}
+
+
+		@Override
+		public void serialize(NullValue value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+			gen.writeNull();
+		}
 	}
 }
